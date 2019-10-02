19e9dc8c20d8d5c2de893d8493130ad5b7c97631
dev0990 <52825199+dev0990@users.noreply.github.com>
2019-10-02 17:59:12 +0200

    Fix :)

--- src/dev0990/Main.php
+++ src/dev0990/Main.php
@@ -27,7 +27,6 @@ class Main extends PluginBase implements Listener
         $this->FormAPI = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
         if(!$this->FormAPI or $this->FormAPI->isDisabled()){
         $this->getLogger()->warning("§cPlugin FormAPI not found, disabling HelpUI by dev0990...");
-        $this->getPluginLoader()->disablePlugin($this);
         }
         $this->getLogger()->info("§aHelpUI by dev0990 enabled!");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
